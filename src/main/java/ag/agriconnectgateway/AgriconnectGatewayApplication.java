5d68af5f39dc54bba188241fc94ac43ee78982ec
Eliott-Tixier <93209602+Eliott-Tixier@users.noreply.github.com>
2024-06-03 14:12:01 +0200

    initalisation gateway

---
+++ src/main/java/ag/agriconnectgateway/AgriconnectGatewayApplication.java
@@ -0,0 +1,13 @@
+package ag.agriconnectgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriconnectGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriconnectGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ag/agriconnectgateway/AgriconnectGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ag.agriconnectgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class AgriconnectGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}*/
