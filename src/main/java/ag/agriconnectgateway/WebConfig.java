9a5f72595796dc0e367360a8cbc6cb3222fd2a49
PAN <paularthur@navaud.fr>
2024-06-18 17:49:23 +0200

    Last one

--- src/main/java/ag/agriconnectgateway/WebConfig.java
+++ src/main/java/ag/agriconnectgateway/WebConfig.java
@@ -1,13 +1,8 @@
 package ag.agriconnectgateway;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @Configuration
 public class WebConfig implements WebFluxConfigurer {
@@ -18,6 +13,6 @@ public class WebConfig implements WebFluxConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
